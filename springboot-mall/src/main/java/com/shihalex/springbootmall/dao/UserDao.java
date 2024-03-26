db8fb182328e145c4857626b1f8890fec025ffa4
7600690 <yan.tai.shih@gmail.com>
2024-03-26 17:31:10 +0800

    帳號功能 - 檢查註冊的 email、隱藏返回密碼，Json 註解使用

--- springboot-mall/src/main/java/com/shihalex/springbootmall/dao/UserDao.java
+++ springboot-mall/src/main/java/com/shihalex/springbootmall/dao/UserDao.java
@@ -7,5 +7,6 @@ import javax.naming.InsufficientResourcesException;
 
 public interface UserDao {
     User getUserById(Integer userId);
+    User getUserByEmail(String email);
     Integer createUser(UserRegisterRequest userRegisterRequest);
 }
--- springboot-mall/src/main/java/com/shihalex/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/shihalex/springbootmall/service/impl/UserServiceImpl.java
@@ -4,22 +4,37 @@ import com.shihalex.springbootmall.dao.UserDao;
 import com.shihalex.springbootmall.dto.UserRegisterRequest;
 import com.shihalex.springbootmall.model.User;
 import com.shihalex.springbootmall.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
-
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        // 檢查註冊的 email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if(user != null){
+            log.warn("該 Email {} 已被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+
+        }
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 
     @Override
     public User getUserById(Integer userId) {
+
         return userDao.getUserById(userId);
     }
 }
